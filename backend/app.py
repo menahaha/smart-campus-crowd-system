60f48dfd4ef4da7ac4ada7ff9618f29b393cf2fd
Ishita Aggarwal <ia3170@srmist.edu.in>
2026-03-06 22:19:27 +0530

    added flask backend

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,19 @@
 print("Backend started") 
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["crowd_management"]
+collection = db["crowd_data"]
+
+@app.route("/")
+def home():
+    return "Crowd Management Backend Running"
+
+if __name__ == "__main__":
+    app.run(debug=True)
